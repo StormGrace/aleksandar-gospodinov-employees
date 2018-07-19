a9d64558ed712ac98647de26368f62c0e227dca8
LordOfTheGulls <galeksanderg@gmail.com>
2018-07-19 04:39:29 +0300

    Formatting Update

--- src/bg/aleksandar/team_longest_period/Statistics.java
+++ src/bg/aleksandar/team_longest_period/Statistics.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -15,25 +16,24 @@ import java.text.SimpleDateFormat;
 
 /* 18/07/2018
  * Assignment Name: "Team Longest Period".
- * Aleksandar Gospodinov, a_gospodinov97@abv.bg
- * GitHub: 
+ * Aleksandar Gospodinov, a_gospodinov97@abv.bg 
  */
 
 public class Statistics {
     private Map<Project, ArrayList<Employee>> database = new HashMap<>();
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     
-    //Read the information from File and Extract it to a proper form.
-    public void readfile(String fileName) {
+    //Reads the Information from a File by Extracting it.
+    public void readFromFile(String fileName) {
 	File file = new File(fileName);
 	 
 	try {
 	    Scanner sc = new Scanner(file);
 	    
 	    while(sc.hasNextLine()) {
-		String[] data = sc.nextLine().replaceAll("NULL", sdf.format(new Date()))
-					     .split("(,\\s)");
-		int days = 0, projectID = 0, employeeID = 0;
+		String[] data = sc.nextLine().replace("NULL", sdf.format(new Date()))
+					     .split("(,[\\s]*)");
+		int dayDifference = 0, projectID = 0, employeeID = 0;
 		Date from = null, to = null;
 		
 		try {
@@ -42,26 +42,25 @@ public class Statistics {
 			
 		    from = sdf.parse(data[2]);
 		    to = sdf.parse(data[3]);
+		    
+		    dayDifference = daysBetween(from, to);
+			
+		    Project project = new Project(projectID);
+		    Employee employee = new Employee(employeeID, dayDifference, from, to);
+			
+		    if(database.containsKey(project)) {	
+			database.get(project).add(employee);
+		    }else {    
+			database.put(project, new ArrayList<Employee>(Arrays.asList(employee)));
+		    }
+		    
 		}catch(Exception e) {
-		    System.out.println("Error Reading Data!");
-		    System.out.println("Skipping Entry...");
-		}
-		
-		days = daydifference(from, to);
-				
-		Project project = new Project(projectID);
-		Employee employee = new Employee(employeeID, days, from, to);
-		
-		if(database.containsKey(project)) {	
-		    database.get(project).add(employee);
-		}else {
-		    ArrayList<Employee> employees = new ArrayList<>();
-		    employees.add(employee);	    
-		    database.put(project, employees);
+		    System.out.println("Error Reading Data!\nSkipping Entry...");
 		}
 	    }
 	    
 	    sc.close();    
+	 
 	}catch(FileNotFoundException e) {
 	    System.out.println("Error! The Required File is either corrupted "
 	    			+ "or doesn't exist!");
@@ -70,29 +69,32 @@ public class Statistics {
 	}
     }
     
-    //Create the Teams and Print the Team with the highest common work period.
-    public void teamhighestyworktime() {
-	 Map<Project, ArrayList<ArrayList<Employee>>> teams = createteams();
+    //Creates the Teams and Prints the Team with the highest common work period.
+    public void byHighestWorktime() {
+	 Map<Project, ArrayList<ArrayList<Employee>>> teams = generateTeams();
  
 	 for(ArrayList<ArrayList<Employee>> team : teams.values()) {
 		Collections.sort(team, new SumWorkTimeComparator());
 	 }
  
-	 List<Entry<Project, ArrayList<ArrayList<Employee>>>> teamsordered = new LinkedList<>(teams.entrySet());
-	 Collections.sort(teamsordered, new LongestPeriodComparator());
+	 List<Entry<Project, ArrayList<ArrayList<Employee>>>> teamSorted = new LinkedList<>(teams.entrySet());
+	 Collections.sort(teamSorted, new LongestPeriodComparator());
 	 
-	 Project project = teamsordered.get(0).getKey();
-	 Employee employee1 = teamsordered.get(0).getValue().get(0).get(0);
-	 Employee employee2 = teamsordered.get(0).getValue().get(0).get(1);
-	 String workPeriod = findworkperiod(employee1, employee2);
+	 Project project = teamSorted.get(0).getKey();
 	 
-	 System.out.println("The TEAM that worked together the most is formed by: Employee[" + employee1.getid() + "] and Employee[" + employee2.getid() + "]"
+	 Employee employee1 = teamSorted.get(0).getValue().get(0).get(0);
+	 Employee employee2 = teamSorted.get(0).getValue().get(0).get(1);
+	 
+	 String workPeriod = workPeriodToString(employee1, employee2);
+	 
+	 System.out.println("The TEAM that worked together the most is formed by: "
+	 	+ "Employee[" + employee1.getid() + "] and Employee[" + employee2.getid() + "]"
 	 	+ " on Project[" + project.getid() + "] for the period [" + workPeriod + "].");
     }
     
-    //Create the teams by a Pair of two Employees who worked together.
-    public Map<Project, ArrayList<ArrayList<Employee>>> createteams() {
-	 Map<Project, ArrayList<ArrayList<Employee>>> teamsdata = new HashMap<>();
+    //Creates the Teams by a Pair of two Employees who worked together.
+    public Map<Project, ArrayList<ArrayList<Employee>>> generateTeams() {
+	 Map<Project, ArrayList<ArrayList<Employee>>> teams = new HashMap<>();
 	 
 	//Mother of Improvisation
 	for(Entry<Project, ArrayList<Employee>> pair : database.entrySet()){
@@ -104,32 +106,30 @@ public class Statistics {
 		for(int i = j + 1; i < pair.getValue().size(); i++) {
 		    Employee employee2 = pair.getValue().get(i);
 		    
-		    if(areinteam(employee1, employee2)) {
-			ArrayList<Employee> teamPair = new ArrayList<>();		
-			teamPair.add(employee1);
-			teamPair.add(employee2);
-	
-			if(teamsdata.containsKey(project)){
-			    teamsdata.get(project).add(teamPair);
+		    if(isTeam(employee1, employee2)) {
+			ArrayList<Employee> teamEmployees = new ArrayList<>(Arrays.asList(employee1, employee2));		
+
+			if(teams.containsKey(project)){
+			    teams.get(project).add(teamEmployees);
 			}else {
-			    ArrayList<ArrayList<Employee>> teams = new ArrayList<>();
-			    teams.add(teamPair);
-			    teamsdata.put(project, teams);
+			    teams.put(project, new ArrayList<>(Arrays.asList(teamEmployees)));
 			}	   
 		    }
 		}
 	    }
 	}
-	return teamsdata;
+	return teams;
     }
     
-    //Output as a String the exact period 2 Employees worked together.
-    private String findworkperiod(Employee emp1, Employee emp2) {
+    //Outputs as a String the exact period 2 Employees worked together.
+    private String workPeriodToString(Employee emp1, Employee emp2) {
 	String date = "";
-	Date from1 = emp1.getdatefrom();
-	Date to1 = emp1.getdateto();
-	Date from2 = emp2.getdatefrom();
-	Date to2 = emp2.getdateto();
+	Date from1, to1, from2, to2;
+	
+	from1 = emp1.getdatefrom();
+	to1 = emp1.getdateto();
+	from2 = emp2.getdatefrom();
+	to2 = emp2.getdateto();
 
 	if(!from1.after(emp2.getdatefrom()) && !to1.before(from2) && !to1.after(to2)){
 	    
@@ -158,12 +158,14 @@ public class Statistics {
 	return date;
     }
     
-    //Compare the Dates between two Employees to see if they worked together.
-    private boolean areinteam(Employee emp1, Employee emp2) {
-	Date from1 = emp1.getdatefrom();
-	Date to1 = emp1.getdateto();
-	Date from2 = emp2.getdatefrom();
-	Date to2 = emp2.getdateto();
+    //Compares the Dates between two Employees to check if they worked together.
+    private boolean isTeam(Employee emp1, Employee emp2) {
+	Date from1, to1, from2, to2;
+	
+	from1 = emp1.getdatefrom();
+	to1 = emp1.getdateto();
+	from2 = emp2.getdatefrom();
+	to2 = emp2.getdateto();
 
     	if(!from1.before(from2) && (!to1.after(to2) || (!from1.after(to2) && !to1.before(to2))) ||
     	   !from1.after(from2) && !to1.before(from2)) {
@@ -173,16 +175,17 @@ public class Statistics {
     	}
     }
     
-    //Return the day difference between 2 Dates [Not the best way of doing it].
-    private int daydifference(Date from, Date to) {
+    //Returns the day difference between 2 Dates [Not the best way of doing it].
+    private int daysBetween(Date from, Date to) {
 	long mils = to.getTime() - from.getTime();
 	long days =  mils / (1000 * 60 * 60 * 24);	
 	return (int)days;
     }
+    
     public static void main(String[] Args) {
 	Statistics statistic = new Statistics();
 	
-	statistic.readfile("data.txt");
-	statistic.teamhighestyworktime();
+	statistic.readFromFile("data.txt");
+	statistic.byHighestWorktime();
     }
 }
